023b1fb9d098ed8bfb9375a344d5262100409468
Michal Puchy <puchy.michal@gmail.com>
2024-01-12 17:06:57 +0100

    3.3: Phonebook backend step3

--- index.js
+++ index.js
@@ -41,3 +41,13 @@ app.get('/info', (request, response) => {
     `
     )
 })
+//Get single person entry
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
